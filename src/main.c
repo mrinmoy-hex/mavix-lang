2453f5c683a5fdec145513932bcee2323060bc83
mrinmoy-hex <ddas38192@gmail.com>
2025-04-13 04:54:23 +0530

    Fix build error and add minimal bytecode disassembler

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+#include "chunk.h"
+#include "debug.h"
+
 int main(int argc, const char* argv[]) {
-    printf("Hello World!\n");
+    Chunk chunk;
+    
+    // Initializing the chunk 
+    initChunk(&chunk);
+
+    // Writing bytes into chunk
+    writeChunk(&chunk, OP_RETURN);
+
+    disassembleChunk(&chunk, "test chunk");
+
+    freeChunk(&chunk);
+
     return 0;
 }
