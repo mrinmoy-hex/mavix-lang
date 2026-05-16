a482116139170781d1477d8029d87dc161e45612
mrinmoy-hex <init2350@gmail.com>
2026-05-17 00:04:38 +0530

    feat(vm): enhance interpret function with bytecode execution and output

--- include/vm.h
+++ include/vm.h
@@ -3,11 +3,23 @@
 
 #include "chunk.h"
 
+// The virtual machine.
 typedef struct {
     Chunk *chunk;
+    uint8_t* ip;    // Instruction pointer
 } VM;
+
+// The result of interpreting some code.
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
 
 void initVM();
 void freeVM();
+
+InterpretResult interpret(Chunk *chunk);
 
 #endif
