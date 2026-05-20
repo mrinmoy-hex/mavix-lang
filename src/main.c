b5aa1f1972ca01d1269ef3254f4f44cecb7dfd48
mrinmoy-hex <init2350@gmail.com>
2026-05-20 10:23:03 +0530

    feat(vm): refactor main function and add REPL and file reading functionality

--- src/main.c
+++ src/main.c
@@ -3,38 +3,81 @@
 #include "debug.h"
 #include "vm.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Read Eval Print Loop
+static void repl() {
+    char line[1024];
+    for (;;) {
+        printf("\x1b[1;36m›\x1b[0m ");
+
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+
+        interpret(line);
+    }
+}
+
+static char* readFile(const char* path) {
+    // Open file in read-bin mode
+    FILE* file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "Could not open file \"%s\" .\n", path);
+        exit(74);       // standard I/O error
+    }
+
+    // seek to the end to measure the total file size
+    fseek(file, 0L, SEEK_END);
+    size_t fileSize = ftell(file);
+    rewind(file);   // reset pointer back to beginnning
+
+    char *buffer = (char*) malloc(fileSize + 1);    // +1 for null terminator
+    if (buffer == NULL) {
+        fprintf(stderr, "Not enough memory to read \"%s\".\n");
+        exit(74);
+    }
+
+    // stream file data into the allocated RAM buffer
+    size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+    if (bytesRead < fileSize) {
+        fprintf(stderr, "Could not read file \"%s\".\n", path);
+        exit(74);
+    }
+    buffer[bytesRead] = '\0';
+
+    fclose(file);
+    return buffer;
+}
+
+static void runFile(const char* path) {
+    char *source = readFile(path);
+    InterpretResult result = interpret(source);
+    free(source);
+
+    if (result == INTERPRET_COMPILE_ERROR)  exit(65);
+    if (result == INTERPRET_RUNTIME_ERROR)  exit(70);
+}
+
+
 int main(int argc, const char* argv[]) {
 
     initVM();
 
-    Chunk chunk;
-    initChunk(&chunk);
+    if (argc == 1) {
+        repl();
+    }
+    else if (argc == 2) {
+        runFile(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: mavix [path]\n");
+        exit(64);
+    }
 
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
-
-    // Test for Binary op
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_ADD, 123);
-
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_DIVIDE, 123);
-
-
-    writeChunk(&chunk, OP_RETURN, 123);
-
-    disassembleChunk(&chunk, "test chunk");
-    interpret(&chunk);
     freeVM();
 
-    freeChunk(&chunk);
     return 0;
 }
